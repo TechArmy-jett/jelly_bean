c4f24e4303243a0272e05bc950f3b02acec38fd1
timilehinjegede <timilehintimothy.jegede@gmail.com>
2020-06-28 07:56:30 +0100

    Starter widgets addition

---
+++ lib/ui/views/home/music_app/songs.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SongsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+            'songs screen'
+        ),
+      ),
+    );
+  }
+}
