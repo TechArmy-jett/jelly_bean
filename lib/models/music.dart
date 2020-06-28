546d067b1a5f22efab9e2cd727f050c03fca3df1
timilehinjegede <timilehintimothy.jegede@gmail.com>
2020-06-28 03:31:37 +0100

    Music list added

---
+++ lib/models/music.dart
@@ -0,0 +1,17 @@
+// list of available music on the device
+class Music {
+  // name of artiste
+  final String artisteName;
+  // title of song
+  final String songTitle;
+  // path of song
+  final String songPath;
+
+  Music({this.artisteName, this.songTitle, this.songPath});
+
+  // list of available songs
+  static List<Music> musicList = [
+
+    // dummy data goes here
+  ];
+}
