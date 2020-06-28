87dfec6feb3274246f9d344857178f8318eb8d48
timilehinjegede <timilehintimothy.jegede@gmail.com>
2020-06-28 05:18:59 +0100

    App Icons added

--- lib/utils/images.dart
+++ lib/utils/images.dart
@@ -6,5 +6,25 @@ class AppImages {
   // no avatar defaults to this dummy avatar images
   static String noAvatar = 'assets/images/';
 
+  // avatars
   static String avatar1 = 'assets/images/';
+
+
+  // app icons
+  static String android = 'assets/images/app_icons/android.png';
+  static String browser = 'assets/images/app_icons/browser.png';
+  static String settings = 'assets/images/app_icons/settings.png';
+  static String calculator = 'assets/images/app_icons/calculator.png';
+  static String calendar = 'assets/images/app_icons/calendar.png';
+  static String camera = 'assets/images/app_icons/camera.png';
+  static String clock = 'assets/images/app_icons/clock.png';
+  static String contact = 'assets/images/app_icons/contact.png';
+  static String downloads = 'assets/images/app_icons/downloads.png';
+  static String email = 'assets/images/app_icons/email.png';
+  static String gallery = 'assets/images/app_icons/gallery.png';
+  static String message = 'assets/images/app_icons/message.png';
+  static String music = 'assets/images/app_icons/music.png';
+  static String phone = 'assets/images/app_icons/phone.png';
+  static String search = 'assets/images/app_icons/search.png';
+
 }
